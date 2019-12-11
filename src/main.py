4e92e4b7c284fe05891280b749232cb6bfb9af1d
janchk <janchk@gmail.com>
2019-12-11 21:17:37 +0300

    Some data preparation. But in unefficient way. So low on memory. Need to optimize.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
-from src.utils import preprocess_data
+from src.utils import DataProcessing
 
 
 if __name__ == "__main__":
-    preprocess_data(data_path="../data/combined_data_1.txt")
+    data_path = "../data/combined_data_1.txt"
+    data_processor = DataProcessing(data_path)
+    data_processor.preprocess_data(0)
--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,78 @@
+import numpy as np
 import pandas as pd
+from sklearn.model_selection import KFold
 
 
-def preprocess_data(data_path):
-    comb_df_1 = pd.read_csv(data_path, names=["User_ID", "Rating", "Date"])
-    print(comb_df_1.shape)
+class DataProcessing:
+    def __init__(self, data_path):
+        self.data_path = data_path
+        # self.movies_col = np.array([])
+        self.num_folds = 5
+        self.main_df = None
+        # self.kf = KFold(n_splits=self.num_folds)
+        # self.data = None
+
+    def preprocess_data(self, fold_num):
+        self.main_df = pd.read_csv(self.data_path, names=["User_ID", "Rating", "Date"])
+        # self.__get_movie_ratings()
+
+        movies_df = self.__get_movie_ratings()
+
+        self.main_df = self.main_df[pd.notnull(self.main_df['Rating'])]
+        self.main_df = self.main_df.reset_index(drop=True)
+        self.main_df["Movie_ID"] = movies_df
+
+        print('Original Shape: {}'.format(self.main_df.shape))
+        self.__clean_data()
+        print('After Trim Shape: {}'.format(self.main_df.shape))
+
+        dfs = self.kf.get_n_splits(self.main_df)
+        # Pivoting Data
+        # pivoted_df = pd.pivot_table(self.main_df, values="Rating", index="User_ID", columns="Movie_ID")
+
+        # print(comb_df.shape)
+
+        # print(comb_df.shape)
+
+        return 0
+
+    def __clean_data(self):
+        f = ['count', 'mean']
+
+        df_movie_summary = self.main_df.groupby('Movie_ID')['Rating'].agg(f)
+        df_movie_summary.index = df_movie_summary.index.map(int)
+        movie_benchmark = round(df_movie_summary['count'].quantile(0.75), 0)
+        drop_movie_list = df_movie_summary[df_movie_summary['count'] < movie_benchmark].index
+
+        print('Movie minimum times of review: {}'.format(movie_benchmark))
+
+        df_user_summary = self.main_df.groupby('User_ID')['Rating'].agg(f)
+        df_user_summary.index = df_user_summary.index.map(int)
+        user_benchmark = round(df_user_summary['count'].quantile(0.75), 0)
+        drop_cust_list = df_user_summary[df_user_summary['count'] < user_benchmark].index
+
+        print('Customer minimum times of review: {}'.format(user_benchmark))
+
+        self.main_df = self.main_df[~self.main_df['Movie_ID'].isin(drop_movie_list)]
+        self.main_df = self.main_df[~self.main_df['User_ID'].isin(drop_cust_list)]
+
+    def __get_movie_ratings(self):
+        temp_movies_col = np.array([])
+        end_df = pd.DataFrame(np.array([["0:", None, None]]), columns=['User_ID', "Rating", "Date"])  # auxiliary appendix
+        dframe_aux = self.main_df.append(end_df, ignore_index=True)
+        null_rat = [i + 1 for i, frame in enumerate(pd.isnull(dframe_aux.Rating)) if frame]
+
+        print(self.main_df.shape)
+        print(len(null_rat))
+
+        for i, curr_val in enumerate(null_rat):
+            try:
+                temp_movies_col = np.append(temp_movies_col, np.full((1, null_rat[i+1] - curr_val - 1), i + 1))
+            except:
+                break
+
+        movies_col = pd.DataFrame(temp_movies_col, columns=["Movie_ID"])
+        return movies_col
 
 
-    return 0
+
